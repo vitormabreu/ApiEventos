1689b9e4aff9d6ce5076e62e56fad59062ecf268
Vitor Abreu <vitinabreu@gmail.com>
2020-04-15 19:00:43 -0300

    Limpa projeto.

--- EventosApp/src/app/app-routing.module.ts
+++ EventosApp/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventoComponent } from './evento/evento.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EventoAddEditComponent } from './evento-add-edit/evento-add-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EventoComponent, pathMatch: 'full' },
+  { path: 'eventos/:id', component: EventosComponent },
+  { path: 'add', component: EventoAddEditComponent },
+  { path: 'eventos/edit/:id', component: EventoAddEditComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
