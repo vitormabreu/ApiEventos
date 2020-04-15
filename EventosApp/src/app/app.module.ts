26ae25d6ff15f8dbfca1f628436b9e0900b9e057
Vitor Abreu <vitinabreu@gmail.com>
2020-04-15 18:52:32 -0300

    Cria o service.

--- EventosApp/src/app/app.module.ts
+++ EventosApp/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { EventoComponent } from './evento/evento.component';
 import { EventoAddEditComponent } from './evento-add-edit/evento-add-edit.component';
+import { EventosService } from './services/eventos.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     EventoComponent,
-    EventoAddEditComponent
+    EventoAddEditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    EventosService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
